f03ffeaeb9de72ab13b382db36b8269098b932af
xMufc <mufcxd@gmail.com>
2022-11-29 09:07:58 +0100

    Zad2

---
+++ Zadanie1/functions.js
@@ -0,0 +1,12 @@
+function idOdd(x){
+    return x % 2 == 0;
+}
+var isEven = (x) => {
+    return x % 2 == 0;
+}
+
+
+console.log(isEven(2));
+console.log(isEven(3));
+console.log(isOdd(2));
+console.log(isOdd(3));
