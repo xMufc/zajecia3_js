c0cf6b4c2fce39efed7e3045f2a6356b1c7db9fd
xMufc <mufcxd@gmail.com>
2022-11-29 11:32:52 +0100

    zad8

--- Zadanie8/zad8.js
+++ Zadanie8/zad8.js
@@ -1,13 +1,10 @@
 let anagram = (a,b) =>{
-    if(a.length === b.length){
-        a = a.split("");
-        b = b.split("");
-        a.sort();
-        b.sort();
-        return a.join("") === b.join("");
-    }
-    else{
-        return false;
-    }   
+    if(a.length !== b.length) return false;
+    
+    a = a.split("");
+    b = b.split("");
+    a.sort();
+    b.sort();
+    return a.join("") === b.join("");
 }
 console.log(anagram("heart", "earth"));
